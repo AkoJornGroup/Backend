96edc71a9d41e4fa1e9ecc399c31da84b0920a19
apkmew <apimook121@gmail.com>
2023-07-22 21:16:48 +0700

    01_GetEventDetails Finished

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from pymongo import MongoClient
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 import requests
+import os
 
 app = FastAPI()
 
-# CORS
+#   Load .env
+load_dotenv( '.env' )
+user = os.getenv( 'user' )
+password = os.getenv( 'password' )
+
+#   Connect to MongoDB
+client = MongoClient(f"mongodb+srv://{user}:{password}@cluster0.dpx3ndy.mongodb.net/")
+db = client['EventBud']
+collection = db['Events']
+
+#   CORS
 origins = ['*']
 
 app.add_middleware(
@@ -22,11 +34,38 @@ app.add_middleware(
 #   Class OOP
 #
 
+class Ticket( BaseModel ):
+    ticketName: str
+    price: int
+
+class Event(BaseModel):
+    eventId: str
+    tag: str
+    name: str
+    startDate: str
+    endDate: str
+    featured: bool
+    info: str
+    location: str
+    tickets: list[Ticket]
+    imageUrl: str
+
 ##############################################################
 #
 #   API
 #
 
+#   Root
 @app.get('/')
 def read_root():
     return {'Hello': 'World'}
+
+#   Get Event Details
+@app.get('/event/{eventId}')
+def get_event( eventId: str ):
+    ''' Get event details by eventId
+        Input: eventId (str)
+        Output: event (dict)
+    '''
+    event = collection.find_one( { 'eventId' : eventId }, { '_id' : 0 } )
+    return event
